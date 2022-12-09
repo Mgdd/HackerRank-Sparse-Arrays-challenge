ab58693f27ad300855b3f571cfe5654a23bc6e0d
Elmigdad Sirry <migdad.sirry@gmail.com>
2022-12-10 03:37:45 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'matchingStrings' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY strings
+     *  2. STRING_ARRAY queries
+     */
+
+    public static List<int> matchingStrings(List<string> strings, List<string> queries)
+    {
+        List<int> result = new List<int>();
+        foreach (var x in queries)
+            result.Add(0);
+        for(int i=0;i<queries.Count;i++)
+        {          
+            for (int j=0;j<strings.Count;j++)
+            {
+                if (strings[j] == queries[i])
+                    result[i]++;
+            }
+        }
+
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        /*int stringsCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> strings = new List<string>();
+
+        for (int i = 0; i < stringsCount; i++)
+        {
+            string stringsItem = Console.ReadLine();
+            strings.Add(stringsItem);
+        }
+
+        int queriesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> queries = new List<string>();
+
+        for (int i = 0; i < queriesCount; i++)
+        {
+            string queriesItem = Console.ReadLine();
+            queries.Add(queriesItem);
+        }
+
+        List<int> res = Result.matchingStrings(strings, queries);
+
+        textWriter.WriteLine(String.Join("\n", res));
+
+        textWriter.Flush();
+        textWriter.Close();*/
+        List<string> strings = new List<string>();
+        List<string> queries= new List<string>();
+        strings.Add("ab");
+        strings.Add("ab");
+        strings.Add("abc");
+        queries.Add("ab");
+        queries.Add("abc");
+        queries.Add("bc");
+        List<int> res = Result.matchingStrings(strings, queries);
+        Console.WriteLine(String.Join("\n", res));
+
+    }
+}
